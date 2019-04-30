c20c981e0d39b5487f94c84f9708f417645b13ac
donle2911 <donle@kainos.com>
2019-04-30 13:27:19 +0100

    JS challenge and solution

---
+++ JSChallenge.js
@@ -0,0 +1,20 @@
+let kainos = 'We develop amazing digital technology that helps people work smarter, better and faster. We create fantastic bespoke solutions for public and private organisations all over the world, and build innovative software platforms that make life easier for our clients and their customers. We offer full lifecycle development and support of innovative, creative and fully-customised solutions, leading digital transformation projects for government and commercial customers. We are also the largest boutique partner in Europe for Workday, ensuring successful implementation for enterprise customers. We provide two market-leading digital platforms. The award-winning Evolve platform helps both NHS and global healthcare providers deliver better outcomes for patients at lower cost. Smart is an awesome automated testing product which helps enterprise clients keep their Workday configuration accurate, reliable and secure.';
+
+let unnecessaryWords = ['amazing', 'fantastic', 'awesome',];
+
+let overusedWords = ['We', 'digital'];
+
+//There is a string called Kainos. First split the string into individual words and save them in a new array called kainosWords.
+
+//There is an array of words that are unnecessary. Iterate over this array to filter out these words. Save the remaining words in an array called kainosEdited.
+
+//There is an array of words called overusedWords. You want to let the user of your program know how many times they have used these overused words.
+
+//You want your program to count the number of sentences in the string Kainos.
+
+//Log the following to console
+//  - Total word count
+//  - word count minus unnecessary words
+//  - number of overused words
+//  - number of sentences
+//  - kainosEdited
